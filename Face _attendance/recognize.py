20c998eb65c14fb795d235c6b9f61c728ab17a67
MirthipatiSujay <118823343+MirthipatiSujay@users.noreply.github.com>
2026-01-28 18:40:06 +0530

    Delete Face _attendance directory

--- Face _attendance/recognize.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def recognize_face(gray_face, recognizer, threshold=55):
-    label, confidence = recognizer.predict(gray_face)
-    if confidence < threshold:
-        return label, confidence
-    return None, confidence
